b9d1cafa67563adc789fec6ffed9b7506710419e
deelcoding <161205351+deelcoding@users.noreply.github.com>
2024-03-24 03:27:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-function sendMail() {
-    var params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
-  
-    const serviceID = "service_e3uxd4h";
-    const templateID = "template_huqc2i9";
-  
-      emailjs.send(serviceID, templateID, params)
-      .then(res=>{
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("message").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
-  
-      })
-      .catch(err=>console.log(err));
-  
-  }
-
   function toggleNavLinksMobile(menu) {
     menu.classList.toggle('open');
   }
