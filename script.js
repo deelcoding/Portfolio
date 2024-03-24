dafb5110979f0ee091e18ea43540998ffadda86d
deelcoding <161205351+deelcoding@users.noreply.github.com>
2024-03-24 01:18:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_e3uxd4h";
+    const templateID = "template_huqc2i9";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
